8bc62d4cd0d25407bfaa4bd58512d8b59ce7977a
Yipeng Sun <syp@umd.edu>
2018-06-25 21:26:40 +0000

    Show root files compressed by 2 different algorithms.

--- demo/write_to_root_file.py
+++ demo/write_to_root_file.py
@@ -13,14 +13,16 @@ test_array = numpy.array([(1, 2.5, 3.4), (4, 5, 6.8)],
                         dtype=[('a', int), ('b', float), ('c', float)])
 
 # Write the new weights to a root tree
-array2root(test_array, 'test_tree.root', 'test_tree', mode='recreate')
+array2root(test_array, 'test_tree-zlib.root', 'test_tree', mode='recreate')
+array2root(test_array, 'test_tree-lzma.root', 'test_tree', mode='recreate',
+           compression='lzma')
 
 # Now read from the newly-created root file, and compare
-test_array_read = root2array('test_tree.root', 'test_tree',
+test_array_read = root2array('test_tree-zlib.root', 'test_tree',
                              branches=['a', 'b', 'c'])
 print('Test if two arrays equal: %s' %
       numpy.array_equal(test_array, test_array_read))
 
 # Now try to update the existing root file
 test_array_new = numpy.array([(3,), (9,)], dtype=[('d', int)])
-array2root(test_array_new, 'test_tree.root', 'test_tree')
+array2root(test_array_new, 'test_tree-zlib.root', 'test_tree')
