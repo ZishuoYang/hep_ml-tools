03c0ba21ef56e60c815273dc5ad438f7675379d0
Yipeng Sun <syp@umd.edu>
2018-06-25 16:36:11 +0000

    Update ROOT file writing: stolen from root_numpy.

--- utils/io.py
+++ utils/io.py
@@ -2,17 +2,34 @@
 
 import ROOT
 from root_numpy import array2tree
+from pathlib import Path
 
 
 def array2root(array, filename,
                treename='tree',
                mode='update', compression='zlib'):
+    # Stolen from 'root_numpy', in:
+    #   root_numpy/src/tree.pyx
+
+    # First, if the file is yet to exist, forcing 'recreate mode'
+    target_file = Path(filename)
+    if not target_file.is_file():
+        mode = 'recreate'
+
     if compression == 'zlib':
-        file = ROOT.TFile(filename, mode, "", 101)
+        rfile = ROOT.TFile(filename, mode, "", 101)
     else:
-        file = ROOT.TFile(filename, mode)
+        rfile = ROOT.TFile(filename, mode)
 
-    tree = array2tree(array, name=treename)
+    if mode == 'update':
+        tree = rfile.Get(treename)
+        datatree = array2tree(array, name=treename, tree=tree)
+    else:
+        datatree = array2tree(array, name=treename)
 
-    file.Write()
-    file.Close()
+    # file.Write("", ROOT.TObject.kOverwrite)
+    datatree.Write(treename, ROOT.TObject.kOverwrite)
+    del datatree
+
+    rfile.Close()
+    del rfile
